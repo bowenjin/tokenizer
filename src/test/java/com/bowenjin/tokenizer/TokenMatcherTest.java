7b730536b7db3cdffdd2d50c084736636b774246
Bowen Jin <bowen.jin@vanderbilt.edu>
2018-10-02 22:29:20 -0700

    Add more tests

--- src/test/java/com/bowenjin/tokenizer/TokenMatcherTest.java
+++ src/test/java/com/bowenjin/tokenizer/TokenMatcherTest.java
@@ -4,6 +4,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import com.bowenjin.regex.InvalidRegexException;
 
+/**
+ * Test space partitions:
+ * regex with concatentation, regex with '*', regex with '|', regex with '+', regex with parenthesis 
+ */
 public class TokenMatcherTest{
   @Test
   public void simple() throws InvalidRegexException{
@@ -11,5 +15,25 @@ public class TokenMatcherTest{
     assertFalse(matcher.process('a'));
     assertFalse(matcher.process('a'));
     assertTrue(matcher.process('a'));
+    assertFalse(matcher.process('a'));
+  }
+   
+  @Test
+  public void multipleMatches() throws InvalidRegexException{
+    TokenMatcher matcher = new TokenMatcher("a*", "a*");
+    assertTrue(matcher.process('a'));
+    assertTrue(matcher.process('a'));
+    assertTrue(matcher.process('a'));
+    assertFalse(matcher.process('b'));
+    assertFalse(matcher.process('a'));
+  }
+ 
+  @Test
+  public void noMatchAfterFirstMismatch() throws InvalidRegexException{
+    TokenMatcher matcher = new TokenMatcher("a*", "a*");
+    assertTrue(matcher.process('a'));
+    assertFalse(matcher.process('b'));
+    assertFalse(matcher.process('a'));
   } 
+ 
 }
