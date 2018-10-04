59b676253cb20a4a15abd2bb7ffb52ee8ea74e8e
Bowen Jin <bowen.jin@vanderbilt.edu>
2018-10-03 22:32:45 -0700

    Add more tests

--- src/test/java/com/bowenjin/tokenizer/TokenizerTest.java
+++ src/test/java/com/bowenjin/tokenizer/TokenizerTest.java
@@ -6,13 +6,15 @@ import java.io.IOException;
 import com.bowenjin.regex.InvalidRegexException;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class TokenizerTest{
   @Test
-  public void longestMatch() throws IOException, InvalidRegexException{
+  public void longestMatch() throws IOException, InvalidRegexException, TokenizerException{
     Map<String, String> map = new HashMap<>();
     map.put("a", "a");
     map.put("aa", "aa");
@@ -28,7 +30,7 @@ public class TokenizerTest{
   }
 
   @Test
-  public void whitespace() throws IOException, InvalidRegexException{
+  public void whitespace() throws IOException, InvalidRegexException, TokenizerException{
     Map<String, String> map = new HashMap<>();
     map.put("space", " ");
     map.put("tab", "\t");
@@ -42,5 +44,57 @@ public class TokenizerTest{
     assertEquals("tab", tokenizer.nextToken().getTokenType());   
     assertEquals("newline", tokenizer.nextToken().getTokenType());   
     assertEquals("return", tokenizer.nextToken().getTokenType());   
+  }
+
+  @Test
+  public void ignoreTokens() throws IOException, InvalidRegexException, TokenizerException{ 
+    Map<String, String> map = new HashMap<>();
+    Set<String> ignoreSet = new HashSet<>();
+    map.put("a", "a");
+    map.put("aa", "aa");
+    ignoreSet.add("aaa");
+    String input = "aaaaaaaa";
+    InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+    Tokenizer tokenizer = new Tokenizer(map, ignoreSet, inputStream);    
+    assertEquals("aa", tokenizer.nextToken().getTokenType());   
+    assertNull(tokenizer.nextToken());
+  }
+
+  @Test
+  public void words() throws IOException, InvalidRegexException, TokenizerException{
+    Map<String, String> map = new HashMap<>();
+    Set<String> ignoreSet = new HashSet<>();
+    map.put("apple", "apple");
+    map.put("banana", "banana");
+    map.put("carrot", "carrot");
+    String input = "apple banana\tcarrot\napple";
+    ignoreSet.add("\t");
+    ignoreSet.add(" ");
+    ignoreSet.add("\n");
+    InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+    Tokenizer tokenizer = new Tokenizer(map, ignoreSet, inputStream);
+    
+    assertEquals("apple", tokenizer.nextToken().getTokenType());
+    assertEquals("banana", tokenizer.nextToken().getTokenType());
+    assertEquals("carrot", tokenizer.nextToken().getTokenType());
+    assertEquals("apple", tokenizer.nextToken().getTokenType());
+  }
+
+
+  @Test(expected=TokenizerException.class)
+  public void doesThrowTokenizerException() throws IOException, InvalidRegexException, TokenizerException{
+    Map<String, String> map = new HashMap<>();
+    Set<String> ignoreSet = new HashSet<>();
+    map.put("a", "a");
+    map.put("b", "b");
+    map.put("c", "c");
+    ignoreSet.add(" ");
+    String input = " a b c\n";
+    InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+    Tokenizer tokenizer = new Tokenizer(map, ignoreSet, inputStream); 
+    assertEquals("a", tokenizer.nextToken().getTokenType());
+    assertEquals("b", tokenizer.nextToken().getTokenType());
+    assertEquals("c", tokenizer.nextToken().getTokenType());
+    tokenizer.nextToken(); //should throw exception
   }
 }
